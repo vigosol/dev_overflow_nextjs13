f0fb894565ab8fb09c01d7a507d0951da5181acd
Muhammad Adnan <vigodesign826@gmail.com>
2023-11-22 13:47:14 +0500

    first test

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Instantly deploy your first Next.js site to a shareable URL with Vercel.</p>
         </a>
       </div>
     </main>
